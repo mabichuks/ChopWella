459681e9be5d392710e1ecb7c3a5b5f7387b25f0
Hassan Liasu <htope68@gmail.com>
2018-06-23 22:05:47 +0100

    [Hcode] Added DateTime prop to checkIn

--- Chopwella.Core/CheckIn.cs
+++ Chopwella.Core/CheckIn.cs
@@ -1,9 +1,12 @@
-﻿namespace Chopwella.Core
+﻿using System;
+
+namespace Chopwella.Core
 {
     public class CheckIn
     {
         public virtual Staff Staff { get; set; }
         public int StaffId { get; set; }
         public bool IsChecked { get; set; }
+        public DateTime DateTime { get; set; }
     }
 }
--- Chopwella.Core/Staff.cs
+++ Chopwella.Core/Staff.cs
@@ -2,6 +2,7 @@
 {
     public class Staff : BaseEntity
     {
+        public string StaffId { get; set; }
         public string Email { get; set; }
         public virtual Category Category { get; set; }
         public int CategoryId { get; set; }
