374dce7c64c743a63363dbd55c8567d5d3f6edb0
gabyslaw <gabyslaw@gmail.com>
2018-07-02 15:08:37 +0100

    Added Edit

---
+++ Chopwella.Web/Controllers/api/UserController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Chopwella.Core;
+using Chopwella.Infrastructure;
+
+namespace Chopwella.Web.Controllers.api
+{
+    //public class UserController : ApiController
+    //{
+    //    private ChopwellaDBContext db = new ChopwellaDBContext();
+
+    //    // GET: api/User
+    //    public IQueryable<Staff> GetStaffs()
+    //    {
+    //        return db.Staffs;
+    //    }
+
+    //    // GET: api/User/5
+    //    [ResponseType(typeof(Staff))]
+    //    public IHttpActionResult GetStaff(int id)
+    //    {
+    //        Staff staff = db.Staffs.Find(id);
+    //        if (staff == null)
+    //        {
+    //            return NotFound();
+    //        }
+
+    //        return Ok(staff);
+    //    }
+
+    //    // PUT: api/User/5
+    //    [ResponseType(typeof(void))]
+    //    public IHttpActionResult PutStaff(int id, Staff staff)
+    //    {
+    //        if (!ModelState.IsValid)
+    //        {
+    //            return BadRequest(ModelState);
+    //        }
+
+    //        if (id != staff.Id)
+    //        {
+    //            return BadRequest();
+    //        }
+
+    //        db.Entry(staff).State = EntityState.Modified;
+
+    //        try
+    //        {
+    //            db.SaveChanges();
+    //        }
+    //        catch (DbUpdateConcurrencyException)
+    //        {
+    //            if (!StaffExists(id))
+    //            {
+    //                return NotFound();
+    //            }
+    //            else
+    //            {
+    //                throw;
+    //            }
+    //        }
+
+    //        return StatusCode(HttpStatusCode.NoContent);
+    //    }
+
+    //    // POST: api/User
+    //    [ResponseType(typeof(Staff))]
+    //    public IHttpActionResult PostStaff(Staff staff)
+    //    {
+    //        if (!ModelState.IsValid)
+    //        {
+    //            return BadRequest(ModelState);
+    //        }
+
+    //        db.Staffs.Add(staff);
+    //        db.SaveChanges();
+
+    //        return CreatedAtRoute("DefaultApi", new { id = staff.Id }, staff);
+    //    }
+
+    //    // DELETE: api/User/5
+    //    [ResponseType(typeof(Staff))]
+    //    public IHttpActionResult DeleteStaff(int id)
+    //    {
+    //        Staff staff = db.Staffs.Find(id);
+    //        if (staff == null)
+    //        {
+    //            return NotFound();
+    //        }
+
+    //        db.Staffs.Remove(staff);
+    //        db.SaveChanges();
+
+    //        return Ok(staff);
+    //    }
+
+    //    protected override void Dispose(bool disposing)
+    //    {
+    //        if (disposing)
+    //        {
+    //            db.Dispose();
+    //        }
+    //        base.Dispose(disposing);
+    //    }
+
+    //    private bool StaffExists(int id)
+    //    {
+    //        return db.Staffs.Count(e => e.Id == id) > 0;
+    //    }
+    //}
+}
