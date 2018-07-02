8fe0126e7b9eb899487e5f38bd87b1d0321a5fdf
mabic <chuks.mabi@yahoo.com>
2018-07-02 10:49:28 +0100

    [Chuks] Completed Auth Module

--- Chopwella.Web/Controllers/AuthController.cs
+++ Chopwella.Web/Controllers/AuthController.cs
@@ -1,17 +1,89 @@
-﻿using System;
+﻿using Chopwella.Web.ViewModels;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using static Chopwella.Infrastructure.Identity.IdentityModel;
 
 namespace Chopwella.Web.Controllers
 {
     public class AuthController : Controller
     {
-        // GET: Auth
-        public ActionResult Index()
+
+        private UserManager<AppUser, int> userMgr;
+        private RoleManager<AppRole, int> roleMgr;
+        public IAuthenticationManager Authmgr => this.HttpContext.GetOwinContext().Authentication;
+
+        public AuthController()
+        {
+            userMgr = Startup.UserManagerFactory.Invoke();
+            roleMgr = Startup.RoleManagerFactory.Invoke();
+        }
+
+        public ActionResult Logout()
+        {
+            var ctxt = this.Request.GetOwinContext();
+            ctxt.Authentication.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
+
+            return Redirect("Login");
+        }
+
+        [HttpGet]
+        public ActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Login(LoginViewModel lvm, string returnUrl)
+        {
+            if (this.ModelState.IsValid)
+            {
+                var user = await userMgr.FindAsync(lvm.UserName, lvm.Password);
+                if (user != null)
+                {
+                    ClaimsIdentity claimsIdentity = await userMgr.CreateIdentityAsync(user, DefaultAuthenticationTypes.ApplicationCookie);
+                    Authmgr.SignIn(claimsIdentity);
+
+                    bool isAdmin = userMgr.IsInRole(user.Id, "Admin");
+
+                    if (isAdmin)
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+
+                }
+                else
+                {
+                    this.ModelState.AddModelError("", "Username or password is invalid");
+                }
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        private string GetRedirectUrl(string returnUrl)
+        {
+            if (string.IsNullOrEmpty(returnUrl) || !Url.IsLocalUrl(returnUrl))
+            {
+                return Url.Action("Index", "Home");
+            }
+
+            return returnUrl;
         }
     }
 }
