6920743355991c85c437e6ca0c20daa27de99b25
mabic <chuks.mabi@yahoo.com>
2018-07-02 10:38:58 +0100

    [Chuks] Second Commit for Authentication

--- Chopwella.Web/Controllers/api/UserController.cs
+++ Chopwella.Web/Controllers/api/UserController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNet.Identity;
+﻿using Chopwella.Web.ViewModels;
+using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
---
+++ Chopwella.Web/ViewModels/UserViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Chopwella.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Chopwella.Web.ViewModels
+{
+    public class UserViewModel
+    {
+        [Required, StringLength(100)]
+        public string UserName { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required, DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required, DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords Do not match")]
+        public string ConfirmPassword { get; set; }
+
+        public int RoleId { get; set; }
+        public Role Role { get; set; }
+
+        public virtual IEnumerable<Role> Roles { get; set; }
+    }
+}
